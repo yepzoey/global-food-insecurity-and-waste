1b408d0e00de4b6a0a95224b7b89e44dbfe05b38
Zoey Zhao <zoey@Zoeys-MacBook-Air.local>
2024-11-22 20:54:02 -0800

    Add GitHub Actions deployment

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html' }),
+		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: '/global-food-insecurity-and-waste'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 	},
 };
